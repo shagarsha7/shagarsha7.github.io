1b9e5a29e5aad23382cf0e9fba5f5d369d7d6dd7
shagarsha7 <shagarsha7@gmail.com>
2019-12-19 02:00:47 -0800

    eigth commit

--- script/script.js
+++ script/script.js
@@ -15,3 +15,13 @@ scrolltop.onclick=function(){
 
     window.scrollTo(0, 0)
 }
+
+function initMap() {
+    // The location of Uluru
+    var uluru = {lat: 8.307531, lng: 77.221837};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 10, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  }
